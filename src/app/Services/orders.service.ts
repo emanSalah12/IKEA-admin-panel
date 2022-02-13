cd53b4d6271c49db0a68bfd9d5670e45f71dde86
EmanSalah95 <seman9918@gmail.com>
2022-02-12 18:41:34 -0800

    get orders fire store

--- src/app/Services/orders.service.ts
+++ src/app/Services/orders.service.ts
@@ -1,147 +1,35 @@
 import { Injectable } from '@angular/core';
 import { IOrder } from '../Models/iorder';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrdersService {
-  orders: IOrder[];
-  constructor() {
-    this.orders = [
-      {
-        CreatedAt: new Date(),
-        Status: false,
-        TotalPrice: 1000,
-        UserID: 'klljkjkj',
-        Items: [
-          {
-            Amount: 5,
-            ProductID: '5454',
-          },
-        ],
-        id:'1'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: true,
-        TotalPrice: 2000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'2'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: false,
-        TotalPrice: 3000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'3'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: true,
-        TotalPrice: 4000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'4'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: false,
-        TotalPrice: 5000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'5'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: false,
-        TotalPrice: 6000,
-        UserID: 'klljkjkj',
-        Items: [
-          {
-            Amount: 5,
-            ProductID: '5454',
-          },
-        ],
-        id:'6'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: true,
-        TotalPrice: 2000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'7'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: false,
-        TotalPrice: 5000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'8'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: true,
-        TotalPrice: 7000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'9'
-      },
-      {
-        CreatedAt: new Date(),
-        Status: false,
-        TotalPrice:8000,
-        UserID: 'ygygy',
-        Items: [
-          {
-            Amount: 3,
-            ProductID: 'lkllh',
-          },
-        ],
-        id:'10'
-      },
-    ];
+  private ordersCollection: AngularFirestoreCollection<IOrder>;
+  orders: Observable<IOrder[]>;
+  constructor(private firestore: AngularFirestore) {
+    this.ordersCollection = firestore.collection<IOrder>('Orders');
+    this.orders = this.ordersCollection.snapshotChanges().pipe(
+      map((actions) => {
+        return actions.map((a: any) => {
+          const data = a.payload.doc.data() as IOrder;
+          data.id = a.payload.doc.id;
+          return data;
+        });
+      })
+    );
+ 
   }
 
-  getOrders(): IOrder[] {
+  getAllOrders(): Observable<IOrder[]> {
     return this.orders;
   }
+
 }
